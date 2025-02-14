7fa483a7d3ed41679d59a85269a8310b355402bf
FramGM <framgm@mail.ru>
2025-02-14 22:26:54 +0300

    get color by pressing Z

--- colorpicker c++/Main.cpp
+++ colorpicker c++/Main.cpp
@@ -1,7 +1,33 @@
 #include <Windows.h>
-
+#include <iostream>
 int main()
 {
+    while (true)
+    {
+        Sleep(10);
+        if (!GetAsyncKeyState('Z'))
+            continue;
 
+        POINT p;
+        COLORREF color;
+        HDC hDC;
+        BOOL b;
+
+        hDC = GetDC(NULL);
+        if (hDC == NULL)
+            continue;
+
+        if (!GetCursorPos(&p))
+            continue;
+
+        color = GetPixel(hDC, p.x, p.y);
+        if (color == CLR_INVALID)
+            continue;
+
+        ReleaseDC(GetDesktopWindow(), hDC);
+
+        printf("%i %i %i\n", GetRValue(color), GetGValue(color), GetBValue(color));
+        Sleep(100);
+    }
 	return 0;
 }
